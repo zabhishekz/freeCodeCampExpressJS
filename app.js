f1afda72d9748af15621e6f34bf3b1c2c12fae29
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 13:24:03 +0530

    JSON Basics-2

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ res.render()
 
 const express = require('express');
 const app = express();
+const {products, people} = require('./data');
 
 app.get('/',(req,res)=>{
-    res.json([{name: 'john'},{name: 'susan'}]);
+    res.json(products);
 })
 
 app.listen(5000,()=>{
