a0a476814981e85adf159ba5bd9107ab9a11c1c4
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 12:00:03 +0530

    Express app and static assets

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-//Express - nodejs web application framework
-//npm i express
-const express  = require('express');
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/',(req,res)=>{
-    console.log('user hit the resource');
-    res.status(200).send('Home Page')
-})
-app.get('/about',(req,res)=>{
-    console.log('user hit the resource');
-    res.status(200).send('About Page')
-})
+app.use(express.static('./public'))
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+
+//adding to static assests 
+//SSR
+/*Now we have removed the app.get for root url and added index.html to public folder.
+so by default the app will always serve the index.html as root file
+*/
+
 app.all('*',(req,res)=>{
-    res.status(404).send('<h1>resource not found</h1>')
+    res.status(404).send('resource not found');
 })
 
 app.listen(5000,()=>{
-    console.log('server is listening on port 5000...');
+    console.log(`server is listening on port ${5000}`);
 });
