af4d0f705c88d3f6210cd99659c098bfbf293ae6
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 19:06:04 +0530

    using Middleware for all routes using app.use

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express()
 const logger = require("./logger")
 //req => middleware => res
 
-app.get('/',logger,(req,res)=>{
+app.use(logger)
+
+app.get('/',(req,res)=>{
     res.send('Home');
 })
-app.get('/about',logger,(req,res)=>{
+app.get('/about',(req,res)=>{
     res.send('About')
 })
+app.get('/api/products',(req,res)=>{
+    res.send('Products');
+})
+app.get('/api/items',(req,res)=>{
+    res.send('About')
+})
+
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
