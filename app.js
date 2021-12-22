70b420f2756e32283527d2102b7340cd9a7f3571
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 20:21:33 +0530

    MEthods- POST, form Example

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 let {people} = require('./data');
 
+//stattic assets
+app.use(express.static('methods-public'));
+//parse form data
+app.use(express.urlencoded({extended: false}))
+
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success: true, data:people})
+})
+
+app.post('/login',(req,res)=>{
+    //console.log(req.body);
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send("POST"); 
 })
 
 app.listen(5000,()=>{
