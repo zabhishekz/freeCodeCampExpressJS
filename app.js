52f0fa270c342e7368f1cac53a9343424d591e2a
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 15:29:52 +0530

    return correction to avoid errors

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get("/api/v1/query",(req,res)=>{
         //res.status(200).send('no products matched your search');
         return res.status(200).json({success: true, data:[]})
     }
-    res.status(200).json(sortedProducts);
+    return res.status(200).json(sortedProducts);
 })
 
 app.listen(5000, () => {
