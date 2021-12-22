0e9b85c885f94e29bd35f5622bf295d179a90694
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 20:42:07 +0530

    Methods- POST(JS Example)

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const app = express();
-let {people} = require('./data');
+const express = require('express')
+const app = express()
+let { people } = require('./data')
 
-//stattic assets
-app.use(express.static('methods-public'));
-//parse form data
-app.use(express.urlencoded({extended: false}))
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-app.get('/api/people',(req,res)=>{
-    res.status(200).json({success: true, data:people})
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
 })
 
-app.post('/login',(req,res)=>{
-    //console.log(req.body);
-    const {name} = req.body;
-    if(name){
-        return res.status(200).send(`Welcome ${name}`)
-    }
-    res.status(401).send("POST"); 
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).send({ success: true, person: name })
 })
 
-app.listen(5000,()=>{
-    console.log('Server is listsening on port 5000...')
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+})
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
