3eeed1a5192a280eb407b5a52ad60dc1da81156a
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 11:42:36 +0530

    Express basics

--- app.js
+++ app.js
@@ -1,52 +1,21 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+//Express - nodejs web application framework
+//npm i express
+const express  = require('express');
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
-  }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
-  }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+app.get('/',(req,res)=>{
+    console.log('user hit the resource');
+    res.status(200).send('Home Page')
+})
+app.get('/about',(req,res)=>{
+    console.log('user hit the resource');
+    res.status(200).send('About Page')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource not found</h1>')
 })
 
-server.listen(5000)
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000...');
+});
