5d02cf4fd1dc7bd5e1d55005db4fc8f4ce809772
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 19:24:07 +0530

    modifying authorize.js middleware

--- authorize.js
+++ authorize.js
@@ -1,6 +1,12 @@
 const auhtorize = (req,res,next) =>{
-    console.log("auhtorize middleware");
-    next();
+    const {user} = req.query;
+    if(user === 'john'){
+        req.user = {name:'john', id:3}
+        next()
+    }
+    else{
+        res.status(401).send('Unauthorized')
+    }
 }
 
 module.exports = auhtorize
