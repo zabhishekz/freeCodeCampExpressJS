b06af1ba44e812568c824a9db15cecf22833d94b
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 19:01:10 +0530

    Setting up middleware in seperate file

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require('express')
 const app = express()
-
+const logger = require("./logger")
 //req => middleware => res
-
-const logger = (req,res,next) =>{
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next();
-}
 
 app.get('/',logger,(req,res)=>{
     res.send('Home');
