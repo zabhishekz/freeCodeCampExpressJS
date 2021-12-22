8be3573f16374cc603a5ef8ef58e0a9a1f13a64f
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 14:43:47 +0530

    Hardcoding for each id

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.get('/api/products',(req,res)=>{
     res.json(newProducts);
 })
 
+app.get('/api/products/1',(req,res)=>{
+    const singleProduct = products.find((product)=> product.id===1)
+    res.json(singleProduct);
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
