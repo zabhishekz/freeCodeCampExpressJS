18b66a619d8ce8605c5daef7713da47587b78974
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-22 19:51:26 +0530

    Methods- GET

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan');
-const logger = require("./logger")
-const authorize = require("./authorize")
-//req => middleware => res
+const express = require('express');
+const app = express();
+let {people} = require('./data');
 
-//1. use ve route
-//2.options- our own / express/ 3rd party
-
-//app.use([logger, authorize])
-//app.use(express.static('./public'))
-app.use(morgan('tiny'))
-
-app.get('/',(req,res)=>{
-    res.send('Home');
-})
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('Products');
-})
-app.get('/api/items',[logger, authorize],(req,res)=>{
-    res.send('About')
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success: true, data:people})
 })
 
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.listen(5000,()=>{
+    console.log('Server is listsening on port 5000...')
 })
