0b19ac48d896e7be4827c1afc1608c896f141c1e
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-23 13:07:57 +0530

    Express Router - Controller -2

--- routes/people.js
+++ routes/people.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 const {getPeople, createPerson, createPersonPostman, updatePerson, deletePerson} = require('../controllers/people')
 
-router.get('/', getPeople)
-router.post('/', createPerson)
-router.post('/postman', createPersonPostman)
-router.put('/:id',updatePerson)
-router.delete('/:id',deletePerson)
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.post('/postman', createPersonPostman)
+// router.put('/:id',updatePerson)
+// router.delete('/:id',deletePerson)
+
+
+//other way to do same thing-
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
+
 module.exports = router;
